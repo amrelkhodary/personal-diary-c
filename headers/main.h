38c592354d0fa9f621180fe0393cf0cad81a04f6
amrelkhodary <amrelkhodarybusiness@gmail.com>
2025-03-29 14:53:52 +0200

    added basic project directory structure

---
+++ headers/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN
+	#define MAIN
+	
+	const char WELCOME_MESSAGE[] = "Welcome to Diary Manager! Please choose one of the options below!\n";
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../headers/main.h"
+int main() {
+	//print a welcome message to the user
+	printf("%s", WELCOME_MESSAGE);
+}
+
